6c9e24210481ccc34589f79a9ecf66a221431178
hridoy <hasanhridot1367@gmail.com>
2022-04-16 13:21:56 -0700

    footer section done

---
+++ src/components/CommonPages/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='bg-dark p-5 mt-5'>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 col-md-8 col-lg-8">
+            <span className='text-light'>CopyRight© 2022 John Smith</span>
+          </div>
+          <div className="col-sm-12 col-md-6 col-lg-4">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/HomePages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='container mt-5 mb-5'>
+      
+    </div>
+  );
+};
+
+export default Home;
