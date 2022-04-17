8e9e34ded09d8b3487a78da3cc359a1399b3dc8b
hridoy <hasanhridot1367@gmail.com>
2022-04-17 15:06:28 -0700

    readme file edit all done

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-## GitHub Link: https://github.com/programming-hero-web-course-4/independent-service-provider-hasanhridoy01,
-## Live Website Link: 
+# Doctor John Smith Personal Website and His Services,
+##### Hello! I make this website this is Single doctor website,a doctor how will his services, and services price and also some details, here....
+##### now i will share how i was make the website and what framework use this project,
+## 1. React BootStrap,
+## 2. React Router,
+## 3. Vanilla Css,
+## 4. Toast,
+## 5. HTML,
+## 6. Authentication,
+####   1. Firebase authentication,
+####   2. Firebase Hooks,
+####   3. Login With Email and Password,
+####   4. Login With Google,
+### GitHub Link: https://github.com/programming-hero-web-course-4/independent-service-provider-hasanhridoy01,
+### Live Website Link: 
--- src/components/HomePages/Details/Details.js
+++ src/components/HomePages/Details/Details.js
@@ -5,7 +5,7 @@ import { DetailsContext } from '../Service/Service';
 import './Details.css';
 
 const Details = () => {
-  const {detailsId} = useParams();
+  const { detailsId } = useParams();
   const name = useContext(<DetailsContext></DetailsContext>);
   return (
     <div className='container details'>
@@ -13,8 +13,37 @@ const Details = () => {
         <div className="col-sm-12 col-md-12 col-lg-6">
           <div className="card border-0 p-2">
             <h3>Labolatory Tests {name} : {detailsId}</h3>
-            <p>Maecenas facilisis metus sed leo aliquam, a accumsan dui posuere. In aliquet pharetra venenatis. Ut ac justo et tortor pharetra dignissim condimentum feugiat velit. Suspendisse potenti.Curabitur velit eros, lobortis eget nisi eget, scelerisque venenatis purus. Vestibulum felis lectus, bibendum in pulvinar sed, suscipit gravida arcu. Sed orci odio, aliquet eget euismod sit amet, porttitor in lacus. Nulla sit amet velit pretium, accumsan enim ut, euismod elit. Nullam consequat metus eget elit ultricies, ut mattis erat rhoncus. Nulla lorem justo, scelerisque sit amet erat at, pulvinar commodo enim. Duis consequat, mi in volutpat euismod, arcu eros venenatis est, quis bibendum elit diam nec enim. Donec consequat diam fermentum nisl egestas, eu lobortis mi facilisis. Integer ante velit, interdum eget est sit amet, aliquet lacinia nibh.</p>
-            <p>$ 100.33</p>
+            <p className='mb-0'>Maecenas facilisis metus sed leo aliquam, a accumsan dui posuere. In aliquet pharetra venenatis. Ut ac justo et tortor pharetra dignissim condimentum feugiat velit. Suspendisse potenti.Curabitur velit eros, lobortis eget nisi eget, scelerisque venenatis purus. Vestibulum felis lectus, bibendum in pulvinar sed, suscipit gravida arcu. Sed orci odio, aliquet eget euismod sit amet, porttitor in lacus. Nulla sit amet velit pretium, accumsan enim ut, euismod elit. Nullam consequat metus eget elit ultricies, ut mattis erat rhoncus. Nulla lorem justo, scelerisque sit amet erat at, pulvinar commodo enim. Duis consequat, mi in volutpat euismod, arcu eros venenatis est, quis bibendum elit diam nec enim.</p>
+            <p className='mt-0'>$ 100.33</p>
+            <button data-bs-toggle="modal" data-bs-target="#exampleModal" className='btn btn-danger w-25'>Add To Cart</button>
+            {/* Modal-Start */}
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">CheckOut</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    <form>
+                      <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Phone Number</label>
+                        <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                      </div>
+                      <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Services Name</label>
+                        <input type="text" class="form-control" />
+                      </div>
+                      <button type="submit" class="btn btn-info">Submit</button>
+                    </form>
+                  </div>
+                  <div className="modal-footer">
+                    <h4>Thank You </h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* Modal-End */}
           </div>
         </div>
         <div className="col-sm-12 col-md-12 col-lg-6">
